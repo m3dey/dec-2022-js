e25c5d032c1a76ebeaeb703e6016b91244c11eb4
m3dey <misteryakk1523@gmail.com>
2023-03-17 15:19:54 +0200

    Mini-project

--- js-additional/script.js
+++ js-additional/script.js
@@ -259,3 +259,47 @@ let deck = [
 // let spadesBiggerThanEight=deck.filter((value)=>value.cardSuit==='spade' && ( value.value==='9' || value.value==='10' || value.value==='jack' || value.value==='queen' || value.value==='king' || value.value==='ace'));
 // console.log(spadesBiggerThanEight);
 
+// setTimeout(()=>{
+//     let x =1;
+//     console.log(x);
+//     setTimeout(()=>{
+//         x++;
+//         console.log(x);
+//     }, 1000)
+// }, 1000)
+
+new Promise((resolve)=>{
+    setTimeout(()=>{
+        let x = 1;
+        console.log(x);
+        resolve(x);
+    },1000);
+})
+    .then((x)=>{
+        return new Promise(resolve => {
+            setTimeout(()=>{
+                x++;
+                console.log(x);
+                resolve(x);
+            },1000)
+        })
+    })
+    .then((x)=>{
+        return new Promise(resolve => {
+            setTimeout(()=>{
+                x++;
+                console.log(x);
+                resolve(x);
+            },1000)
+        })
+    })
+    .then((x)=>{
+        return new Promise(resolve => {
+            setTimeout(()=>{
+                x++;
+                console.log(x);
+                resolve(x);
+            },1000)
+        })
+    })
+
